386cf37b3be4722d5c8585e4fb82c79fd35885d3
thisellie <81550623+thisellie@users.noreply.github.com>
2025-01-30 20:15:28 +0800

    add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function App() {
   const [cart, setCart] = useState([])
@@ -10,9 +11,9 @@ function App() {
     <>
       <Nav cart={cart.length} />
       <Outlet context={[cart, setCart]} />
+      <Footer />
     </>
   )
 }
 
 export default App
-
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        🧑‍💻 by{' '}
+        <a href='https://github.com/thisellie' target='_blank'>
+          thisellie
+        </a>
+      </p>
+    </footer>
+  )
+}
