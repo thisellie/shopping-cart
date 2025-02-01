547156fad7ee449694866c507fbf93bda5f3306a
thisellie <81550623+thisellie@users.noreply.github.com>
2025-02-01 22:35:40 +0800

    style `Nav` component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,38 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { ShoppingCart } from 'lucide-react'
+import { ShoppingCart, Menu } from 'lucide-react'
 import './Nav.css'
 
 function Nav({ cart }) {
   return (
     <nav>
+      <button id='menu'>
+        <Menu strokeWidth={1} />
+      </button>
       <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/shop'>Shop</Link>
-        </li>
-        <li>
-          <Link to='/cart'>Cart</Link>
-        </li>
+        <Link to='/'>
+          <li>Featured</li>
+        </Link>
+        <Link to='/shop'>
+          <li>Browse</li>
+        </Link>
+        <li>Men</li>
+        <li>Women</li>
       </ul>
-      <ShoppingCart />
-      <p>{cart}</p>
+      <h1>
+        <Link to='/'>thisellie&apos;s shop</Link>
+      </h1>
+      <div>
+        <Link to='/cart'>
+          <ShoppingCart strokeWidth={1} />
+          <div id='cart-count'>
+            <p>{cart}</p>
+          </div>
+        </Link>
+        <Link to='/cart'>
+          <p>Cart</p>
+        </Link>
+      </div>
     </nav>
   )
 }
