0ee405df9231cc2f8fbcaeb3863e129c09a7e0c3
thisellie <81550623+thisellie@users.noreply.github.com>
2025-03-18 16:23:08 +0800

    add CartItem component and update Cart page layout

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,9 @@
+import './CartItem.css'
+
+export default function CartItem() {
+  return (
+    <>
+      <div className='container'></div>
+    </>
+  )
+}
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,19 @@
+import { useOutletContext } from 'react-router-dom'
 import './Cart.css'
+import CartItem from '../components/CartItem'
+import Card from '../components/Card'
 
 function Cart() {
+  const [cart] = useOutletContext()
   return (
     <>
-      <main>
-        <h1>Cart</h1>
+      <main id='cart'>
+        <CartItem></CartItem>
+        <CartItem></CartItem>
+        <CartItem></CartItem>
+        {/* {cart.map(product => (
+          <Card key={product.id} product={product}></Card>
+        ))} */}
       </main>
     </>
   )
