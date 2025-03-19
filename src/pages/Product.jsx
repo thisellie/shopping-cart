75c58a305655f8571df32baa12723a12f36181b5
thisellie <81550623+thisellie@users.noreply.github.com>
2025-03-19 17:19:28 +0800

    add quantity selection to Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
-import { useOutletContext, useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import './Product.css'
+import { useState, useEffect } from 'react'
+import { useOutletContext, useParams } from 'react-router-dom'
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa'
 import Loading from '../components/Loading'
 
@@ -17,9 +17,15 @@ export default function Product() {
   const { productId } = useParams()
   const [product, setProduct] = useState(null)
   const [cart, setCart] = useOutletContext()
+  const [quantity, setQuantity] = useState(1)
 
   function addToCart(product) {
-    setCart([...cart, product])
+    const item = cart.find(item => item.id === product.id)
+    if (item) {
+      item.quantity += quantity
+    } else {
+      setCart([...cart, { ...product, quantity }])
+    }
   }
 
   useEffect(() => {
@@ -35,19 +41,30 @@ export default function Product() {
 
   return (
     // TODO: Add loading skeleton
-    <main id='product'>
-      <img className='image' src={product.image} alt={product.title} />
-      <div className='product-details'>
-        <h1 className='title'>{product.title}</h1>
-        <p className='rate'>{getStars(product.rating.rate)}</p>
-        <h2 className='price'>${product.price}</h2>
-        <p className='description'>{product.description}</p>
-        {/* <p className='category'>{product.category}</p> */}
-        <p className='count'>
-          <b>Quantity: </b>
-          {product.rating.count}
+    <main id="product">
+      <img className="image" src={product.image} alt={product.title} />
+      <div className="product-details">
+        <h1 className="title">{product.title}</h1>
+        <p className="rate">{getStars(product.rating.rate)}</p>
+        <h2 className="price">${product.price}</h2>
+        <p className="description">{product.description}</p>
+        <p className="quantity">
+          <span><b style={{ marginRight: '1rem' }}>Quantity</b></span>
+          <button className="quantity-button" onClick={() => setQuantity(quantity - 1)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8z" />
+            </svg>
+          </button>
+          <input className="quantity-input" type="number" value={quantity} onChange={e => setQuantity(Number(e.target.value))} />
+          <button className="quantity-button" onClick={() => setQuantity(quantity + 1)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+          </button>
         </p>
-        <button onClick={() => addToCart(product)}>Add To Cart</button>
+        <button className="add-to-cart" onClick={() => addToCart(product)}>
+          Add To Cart
+        </button>
       </div>
     </main>
   )
