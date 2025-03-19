4fdeaa0027c82ab28eb1b3afa5b5ec013efaef03
thisellie <81550623+thisellie@users.noreply.github.com>
2025-03-19 17:19:16 +0800

    update Cart and CartItem layout

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,27 @@
 import './CartItem.css'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-export default function CartItem() {
+export default function CartItem({ product }) {
   return (
-    <>
-      <div className='container'></div>
-    </>
+    <Link to={`/shop/${product.id}`} className='cart-item'>
+      <img src={product.image} alt={product.title} className="cart-item-image" />
+      <div className="cart-item-details">
+        <h3 className="cart-item-title">{product.title}</h3>
+        <p className="cart-item-price">${product.price.toFixed(2)}</p>
+        <p className="cart-item-quantity">Quantity: {product.quantity}</p>
+      </div>
+    </Link>
   )
 }
+
+CartItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+  }).isRequired,
+}
+
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,21 +1,15 @@
 import { useOutletContext } from 'react-router-dom'
 import './Cart.css'
 import CartItem from '../components/CartItem'
-import Card from '../components/Card'
 
 function Cart() {
   const [cart] = useOutletContext()
   return (
-    <>
       <main id='cart'>
-        <CartItem></CartItem>
-        <CartItem></CartItem>
-        <CartItem></CartItem>
-        {/* {cart.map(product => (
-          <Card key={product.id} product={product}></Card>
-        ))} */}
+        {cart.map(product => (
+          <CartItem key={product.id} product={product}></CartItem>
+        ))}
       </main>
-    </>
   )
 }
 
