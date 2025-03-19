58dba82f5a5bb882e9239875165de35530172497
thisellie <81550623+thisellie@users.noreply.github.com>
2025-03-19 17:57:40 +0800

    add mobile navigation menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,65 +1,61 @@
+import './Nav.css'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { ShoppingCart, Menu } from 'lucide-react'
-import './Nav.css'
+import { ShoppingCart, Menu, X } from 'lucide-react'
 import { useState } from 'react'
 
 function Nav({ cart }) {
   const [showMenu, setShowMenu] = useState(false)
 
-  function updateMenuState() {
-    let temp = !showMenu
-    setShowMenu(temp)
-    console.log(showMenu)
+  function toggleMenu() {
+    setShowMenu(!showMenu)
   }
 
   return (
     <>
       <nav>
-        <button id='menu-icon' onClick={updateMenuState}>
-          <Menu strokeWidth={1} />
+        <button id="menu-icon" onClick={toggleMenu}>
+          {showMenu ? <X /> : <Menu color="#010101" />}
         </button>
-        <ul>
-          <Link to='/'>
+        <ul className="desktop-menu">
+          <Link to="/">
             <li>Featured</li>
           </Link>
-          <Link to='/shop'>
+          <Link to="/shop">
             <li>Browse</li>
           </Link>
-          <li>Men</li>
-          <li>Women</li>
         </ul>
         <h1>
-          <Link to='/'>thisellie&apos;s shop</Link>
+          <Link to="/">thisellie&apos;s shop</Link>
         </h1>
         <div>
-          <Link to='/cart'>
+          <Link to="/cart">
             <ShoppingCart strokeWidth={1} />
-            <div id='cart-count'>
+            <div id="cart-count">
               <p>{cart}</p>
             </div>
           </Link>
-          <Link to='/cart'>
+          <Link to="/cart">
             <p>Cart</p>
           </Link>
         </div>
       </nav>
-      <div id='menu'>
-        <ul>
-          <Link to='/'>
-            <li>Featured</li>
-          </Link>
-          <Link to='/shop'>
-            <li>Browse</li>
-          </Link>
-          <li>Men</li>
-          <li>Women</li>
-          <hr />
-          <Link to='/cart'>
-            <li>Cart</li>
-          </Link>
-        </ul>
-      </div>
+      {showMenu && (
+        <div id="menu" className="mobile-menu">
+          <ul>
+            <Link to="/" onClick={toggleMenu}>
+              <li>Featured</li>
+            </Link>
+            <Link to="/shop" onClick={toggleMenu}>
+              <li>Browse</li>
+            </Link>
+            <hr />
+            <Link to="/cart" onClick={toggleMenu}>
+              <li>Cart</li>
+            </Link>
+          </ul>
+        </div>
+      )}
     </>
   )
 }
